56233e53a2aa52926f4f409c639219a2a892e8be
Briathna Bubick <149510120+kamyb15@users.noreply.github.com>
2026-04-09 14:40:55 -0400

    update navigation layout

--- App/(tabs)/_layout.tsx
+++ App/(tabs)/_layout.tsx
@@ -5,27 +5,30 @@ export default function TabLayout() {
   return (
     <Tabs screenOptions={{ 
       tabBarActiveTintColor: '#E50914', // Netflix Red
-      tabBarStyle: { backgroundColor: '#000', borderTopWidth: 0 } 
+      tabBarStyle: { backgroundColor: '#2e2e2e' } 
     }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
+          tabBarIconStyle: { marginTop: 15},
+          title: '',
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={30} color={color} />,
         }}
       />
       <Tabs.Screen
         name="messages"
         options={{
-          title: 'Messages',
-          tabBarIcon: ({ color }) => <Ionicons name="paper-plane" size={24} color={color} />,
+          tabBarIconStyle: { marginTop: 15},
+          title: '',
+          tabBarIcon: ({ color }) => <Ionicons name="paper-plane" size={30} color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
+          tabBarIconStyle: { marginTop: 15},
+          title: '',
+          tabBarIcon: ({ color }) => <Ionicons name="person" size={30} color={color} />,
         }}
       />
     </Tabs>
